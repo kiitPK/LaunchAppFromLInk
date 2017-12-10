dface8df6cec123db9777ea831aef92a235b50ca
kiitPK <prakashsingh184@gmail.com>
2017-12-10 14:12:33 +0530

    --

---
+++ app/src/androidTest/java/applink/launch/com/launchappfromlink/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package applink.launch.com.launchappfromlink;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("applink.launch.com.launchappfromlink", appContext.getPackageName());
+    }
+}
